3a8c7860d0a60c4bec4a286f9d70dd6fd4c8cd15
Ibrahim Mbaziira <code.ibra@gmail.com>
2018-01-08 11:25:16 +0300

    Initial Commit

---
+++ extension-dance-party/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  extension-dance-party
+//
+//  Created by Ibrahim Mbaziira on 1/8/18.
+//  Copyright © 2018 Ibrahim Mbaziira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
